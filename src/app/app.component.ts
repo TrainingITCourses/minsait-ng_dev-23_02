ad618babe8a5370ff7345bccdbd10e8873e6f353
Alberto Basalo <albertobasalo@hotmail.com>
2023-03-01 18:58:57 +0100

    feat: templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  activities = [
+    {
+      title: 'Activity 1',
+      date: '2029-01-01',
+      location: 'New York',
+      price: 100,
+    },
+    {
+      title: 'Activity 2',
+      date: '2029-01-02',
+      location: 'London',
+      price: 200,
+    },
+  ];
+}
