e720ccadffe615a6e2bb2719fd16a9488afe4b9e
Alberto Basalo <albertobasalo@hotmail.com>
2023-03-08 19:02:23 +0100

    feat: shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ActivityItemComponent } from './activity-item/activity-item.component';
 import { LinkComponent } from './link/link.component';
 
 @NgModule({
   declarations: [LinkComponent, ActivityItemComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [LinkComponent, ActivityItemComponent],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
+  exports: [
+    LinkComponent,
+    ActivityItemComponent,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
 })
 export class SharedModule {}
