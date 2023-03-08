d7364cf909929b8511e4a2b5d091497492f4d9a6
Alberto Basalo <albertobasalo@hotmail.com>
2023-03-08 18:52:34 +0100

    feat: login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  pageTitle = 'Login page';
+
+  loginForm: FormGroup = this.fb.group({
+    email: '',
+    password: '1234',
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onRegister() {
+    console.log('sending data to server', this.loginForm.value);
+  }
+}
