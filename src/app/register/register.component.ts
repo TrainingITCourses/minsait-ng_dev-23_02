372e55810ecb7ecb9ba0c6cc246d3787da86f21c
Alberto Basalo <albertobasalo@hotmail.com>
2023-03-08 18:35:29 +0100

    feat: register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  pageTitle = 'Register page';
 
+  formGroup: FormGroup = this.formBuilder.group({
+    fullName: '',
+    email: '',
+    password: '1234',
+    repeatedPassword: '1234',
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onRegister() {
+    console.log('sending data to server', this.formGroup.value);
+  }
 }
